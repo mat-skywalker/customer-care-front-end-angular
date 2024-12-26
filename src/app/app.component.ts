9b46112ed9d89adb9f38a24ea441fd86fb50d323
mat-skywalker <thanedrell07@hotmail.com>
2024-12-26 11:50:55 +0100

    Fix to load local images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ActionComponent} from "./components/action/action.component";
+import {HeaderComponent} from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ActionComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'customer-care-front-end-angular';
+  title = "customer-care-front-end-angular";
+  logoUrl = "../assets/client-network.gif";
 }
